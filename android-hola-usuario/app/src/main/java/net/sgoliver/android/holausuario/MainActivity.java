1d34714336b171419ad97ad4e93b414b1c92c9ce
Salvador Gomez <sgoliver.net@gmail.com>
2015-01-16 19:53:36 +0100

    Hola Usuario

---
+++ android-hola-usuario/app/src/main/java/net/sgoliver/android/holausuario/MainActivity.java
@@ -0,0 +1,68 @@
+package net.sgoliver.android.holausuario;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends ActionBarActivity {
+
+    private EditText txtNombre;
+    private Button btnAceptar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Obtenemos una referencia a los controles de la interfaz
+        txtNombre = (EditText)findViewById(R.id.TxtNombre);
+        btnAceptar = (Button)findViewById(R.id.BtnAceptar);
+
+        //Implementamos el evento “click” del botón
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creamos el Intent
+                Intent intent =
+                        new Intent(MainActivity.this, SaludoActivity.class);
+
+                //Creamos la información a pasar entre actividades
+                Bundle b = new Bundle();
+                b.putString("NOMBRE", txtNombre.getText().toString());
+
+                //Añadimos la información al intent
+                intent.putExtras(b);
+
+                //Iniciamos la nueva actividad
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ android-hola-usuario/app/src/main/java/net/sgoliver/android/holausuario/SaludoActivity.java
@@ -0,0 +1,49 @@
+package net.sgoliver.android.holausuario;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class SaludoActivity extends ActionBarActivity {
+
+    private TextView txtSaludo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saludo);
+
+        //Localizar los controles
+        txtSaludo = (TextView)findViewById(R.id.TxtSaludo);
+
+        //Recuperamos la información pasada en el intent
+        Bundle bundle = this.getIntent().getExtras();
+
+        //Construimos el mensaje a mostrar
+        txtSaludo.setText("Hola " + bundle.getString("NOMBRE"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_saludo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
