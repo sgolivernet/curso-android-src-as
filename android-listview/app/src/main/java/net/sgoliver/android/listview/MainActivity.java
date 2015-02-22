5460026555bf03ee6d04dc5de2e9bbfdc7006ab5
Salvador Gomez <sgoliver.net@gmail.com>
2015-02-22 13:06:16 +0100

    ListView

---
+++ android-listview/app/src/main/java/net/sgoliver/android/listview/MainActivity.java
@@ -0,0 +1,128 @@
+package net.sgoliver.android.listview;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+    private TextView lblEtiqueta;
+    private ListView lstOpciones;
+
+    private Titular[] datos =
+            new Titular[]{
+                    new Titular("Título 1", "Subtítulo largo 1"),
+                    new Titular("Título 2", "Subtítulo largo 2"),
+                    new Titular("Título 3", "Subtítulo largo 3"),
+                    new Titular("Título 4", "Subtítulo largo 4"),
+                    new Titular("Título 5", "Subtítulo largo 5"),
+                    new Titular("Título 6", "Subtítulo largo 6"),
+                    new Titular("Título 7", "Subtítulo largo 7"),
+                    new Titular("Título 8", "Subtítulo largo 8"),
+                    new Titular("Título 9", "Subtítulo largo 9"),
+                    new Titular("Título 10", "Subtítulo largo 10"),
+                    new Titular("Título 11", "Subtítulo largo 11"),
+                    new Titular("Título 12", "Subtítulo largo 12"),
+                    new Titular("Título 13", "Subtítulo largo 13"),
+                    new Titular("Título 14", "Subtítulo largo 14"),
+                    new Titular("Título 15", "Subtítulo largo 15")};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Ejemplo básico
+        //final String[] datos =
+        //        new String[]{"Elem1","Elem2","Elem3","Elem4","Elem5"};
+        //
+        //ArrayAdapter<String> adaptador =
+        //        new ArrayAdapter<String>(this,
+        //                android.R.layout.simple_list_item_1, datos);
+        //
+        //lstOpciones = (ListView)findViewById(R.id.LstOpciones);
+        //
+        //lstOpciones.setAdapter(adaptador);
+
+        lblEtiqueta = (TextView)findViewById(R.id.LblEtiqueta);
+        lstOpciones = (ListView)findViewById(R.id.LstOpciones);
+
+        //Cabecera
+        View header = getLayoutInflater().inflate(R.layout.list_header, null);
+        lstOpciones.addHeaderView(header);
+
+        //Adaptador
+        AdaptadorTitulares adaptador =
+                new AdaptadorTitulares(this, datos);
+
+        lstOpciones.setAdapter(adaptador);
+
+        //Eventos
+        lstOpciones.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+
+                //Alternativa 1:
+                String opcionSeleccionada =
+                        ((Titular)a.getItemAtPosition(position)).getTitulo();
+
+                //Alternativa 2:
+                //String opcionSeleccionada =
+                //		((TextView)v.findViewById(R.id.LblTitulo))
+                //			.getText().toString();
+
+                lblEtiqueta.setText("Opción seleccionada: " + opcionSeleccionada);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class AdaptadorTitulares extends ArrayAdapter<Titular> {
+
+        public AdaptadorTitulares(Context context, Titular[] datos) {
+            super(context, R.layout.listitem_titular, datos);
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            View item = inflater.inflate(R.layout.listitem_titular, null);
+
+            TextView lblTitulo = (TextView)item.findViewById(R.id.LblTitulo);
+            lblTitulo.setText(datos[position].getTitulo());
+
+            TextView lblSubtitulo = (TextView)item.findViewById(R.id.LblSubTitulo);
+            lblSubtitulo.setText(datos[position].getSubtitulo());
+
+            return(item);
+        }
+    }
+}
