ca88dcbd9ebec3daa94e6a2bde827893a6f6f501
Salvador Gomez <sgoliver.net@gmail.com>
2015-02-24 19:32:54 +0100

    GridView

---
+++ android-gridview/app/src/main/java/net/sgoliver/android/gridview/MainActivity.java
@@ -0,0 +1,66 @@
+package net.sgoliver.android.gridview;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+    private TextView lblMensaje;
+    private GridView grdOpciones;
+
+    private String[] datos = new String[50];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for(int i=1; i<=50; i++)
+            datos[i-1] = "Dato " + i;
+
+        ArrayAdapter<String> adaptador =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datos);
+
+        lblMensaje = (TextView)findViewById(R.id.LblMensaje);
+        grdOpciones = (GridView)findViewById(R.id.GridOpciones);
+
+        grdOpciones.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> parent,
+                                            android.view.View v, int position, long id) {
+                        lblMensaje.setText("Opción seleccionada: "
+                                + parent.getItemAtPosition(position));
+                    }
+                });
+
+        grdOpciones.setAdapter(adaptador);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
