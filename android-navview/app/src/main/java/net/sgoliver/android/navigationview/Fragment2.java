8bd0af433bb4f0239f420aa50132ce9fded6d975
Salvador Gomez <sgoliver.net@gmail.com>
2015-07-05 17:56:51 +0200

    Navigation Drawer (NavigationView)

---
+++ android-navview/app/src/main/java/net/sgoliver/android/navigationview/Fragment2.java
@@ -0,0 +1,21 @@
+package net.sgoliver.android.navigationview;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment2 extends Fragment {
+
+    public Fragment2() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+    }
+}
---
+++ android-navview/app/src/main/java/net/sgoliver/android/navigationview/MainActivity.java
@@ -0,0 +1,125 @@
+package net.sgoliver.android.navigationview;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar appbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        appbar = (Toolbar)findViewById(R.id.appbar);
+        setSupportActionBar(appbar);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_nav_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        /*
+        //Eventos del Drawer Layout
+        drawerLayout.setDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+        */
+
+        navView = (NavigationView)findViewById(R.id.navview);
+        navView.setNavigationItemSelectedListener(
+            new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                    boolean fragmentTransaction = false;
+                    Fragment fragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.menu_seccion_1:
+                            fragment = new Fragment1();
+                            fragmentTransaction = true;
+                            break;
+                        case R.id.menu_seccion_2:
+                            fragment = new Fragment2();
+                            fragmentTransaction = true;
+                            break;
+                        case R.id.menu_seccion_3:
+                            fragment = new Fragment3();
+                            fragmentTransaction = true;
+                            break;
+                        case R.id.menu_opcion_1:
+                            Log.i("NavigationView", "Pulsada opción 1");
+                            break;
+                        case R.id.menu_opcion_2:
+                            Log.i("NavigationView", "Pulsada opción 2");
+                            break;
+                    }
+
+                    if(fragmentTransaction) {
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.content_frame, fragment)
+                                .commit();
+
+                        menuItem.setChecked(true);
+                        getSupportActionBar().setTitle(menuItem.getTitle());
+                    }
+
+                    drawerLayout.closeDrawers();
+
+                    return true;
+                }
+            });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
